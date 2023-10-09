8c2513026d2234f97b91d4044d06f1c7b32b99e6
Reshma5245 <147255169+Reshma5245@users.noreply.github.com>
2023-10-09 09:33:13 +0530

    Update class.c
    Deleted copyright texts

--- class.c
+++ class.c
@@ -1,12 +1,3 @@
-/*    class.c
- *
- *    Copyright (C) 2022 by Paul Evans and others
- *
- *    You may distribute under the terms of either the GNU General Public
- *    License or the Artistic License, as specified in the README file.
- *
- */
-
 /* This file contains the code that implements perl's new `use feature 'class'`
  * object model
  */
